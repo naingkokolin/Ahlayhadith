5e5c9c9b40562bff67e5cd2a37981384a2bad2d1
naingkokolin <naingkokolin007@gmail.com>
2026-05-04 14:51:23 +0630

    added admin

---
+++ admin/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return personalInfo();
+}
+
+function personalInfo() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
