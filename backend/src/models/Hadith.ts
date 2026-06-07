d5dc58372471cd6258d3f00f9fc8345d7be1b4ff
naingkokolin <naingkokolin007@gmail.com>
2026-06-07 13:42:40 +0630

    changed api

--- backend/src/models/Hadith.ts
+++ backend/src/models/Hadith.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document } from "mongoose";
+import mongoose from "mongoose";
 
 export interface IHadith {
   // remove extends Document
@@ -13,11 +13,6 @@ export interface IHadith {
 
 const hadithSchema = new mongoose.Schema<IHadith>(
   {
-    // bible: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: "HadithBible",
-    //   required: true,
-    // },
     book: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "HadithBook",
@@ -53,7 +48,6 @@ const hadithSchema = new mongoose.Schema<IHadith>(
 hadithSchema.index({ text_mm: "text" });
 // Each hadith_number unique within a book
 hadithSchema.index({ book: 1, hadith_number: 1 }, { unique: true });
-hadithSchema.index({ bible: 1, chapter: 1 });
 
 const Hadith = mongoose.model<IHadith>("Hadith", hadithSchema);
 export default Hadith;
